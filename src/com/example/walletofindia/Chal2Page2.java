0a9d360fb157e71f1003481418607f52db06bd4a
Aravind-Suresh <arvindsuresh2009@gmail.com>
2014-12-20 10:39:45 +0530

    -a

---
+++ src/com/example/walletofindia/Chal2Page2.java
@@ -0,0 +1,63 @@
+package com.example.walletofindia;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Chal2Page2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_chal2_page2);
+		
+		 /*for(int i=0;i<9;i++)
+		 {
+			 try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
+			 TextView ti = (TextView) findViewById(R.id.secleft);
+			 ti.setText((Integer.parseInt(ti.getText().toString())-1)+"");
+		 }*/
+		
+		/*long t1=System.currentTimeMillis(),t2;
+		int x=1,c=0;
+		
+		while(true)
+		{
+			if(x==1){
+				t1=System.currentTimeMillis();
+				x=0;
+			}
+			t2=System.currentTimeMillis();
+			if(t2-t1>1000)
+			{
+				TextView ti = (TextView) findViewById(R.id.secleft);
+				ti.setText((Integer.parseInt(ti.getText().toString())-1)+"");
+				Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_SHORT).show();
+				x=1;
+				c++;
+			}
+			if(c==9)	break;
+		}*/
+		 
+		 Intent intent = new Intent(getApplicationContext(),Chal2Page3.class);
+		 startActivity(intent);
+		 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.chal2_page2, menu);
+		return true;
+	}
+
+}
